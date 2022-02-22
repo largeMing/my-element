08288161c54169ae9c8ecab4e12f931a5366b5a2
Aming <814067990@qq.com>
2022-02-23 00:03:20 +0800

    feat(elements): add new components - container

---
+++ src/components/container/Aside.vue
@@ -0,0 +1,30 @@
+<template>
+    <aside class="el-aside" :style="{width}">
+        <slot/>
+    </aside>
+</template>
+
+<script lang="ts">
+export default {
+    name: "ElAside"
+}
+</script>
+
+<script lang="ts" setup>
+import {withDefaults} from 'vue'
+
+interface Props {
+    width?:string
+}
+withDefaults(defineProps<Props>(), {
+    width: "300px"
+})</script>
+
+<style lang="scss">
+@import "../style/mixin";
+@include b(aside) {
+    overflow: auto;
+    box-sizing: border-box;
+    flex-shrink: 0;
+}
+</style>
---
+++ src/components/container/Main.vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="el-main">
+        <slot/>
+    </main>
+</template>
+
+<script lang="ts">
+export default {
+    name: "ElMain"
+}
+</script>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss">
+@import "../style/mixin";
+@include b(main) {
+    display: flex;
+    flex: 1;
+    flex-basis: auto;
+    overflow: auto;
+    box-sizing: border-box;
+    padding: $--main-padding;
+}
+</style>
